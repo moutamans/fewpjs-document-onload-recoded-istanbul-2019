0641cc34dfed2846a643787565c90900a853376f
https://learn.co/moutaman <moutaman.sabbagh@gmail.com>
2020-04-27 18:00:40 +0300

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // Your code goes here
+
+document.addEventListener("DOMContentLoaded", function() {
+    let pText = document.getElementById("p#text")
+    text.innerHTML = "This is really cool!";    
+  });
