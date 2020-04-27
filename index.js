0f4c75da83002921bfaac9b1461c678e5f23f3ae
https://learn.co/moutaman <moutaman.sabbagh@gmail.com>
2020-04-27 18:10:29 +0300

    Done.

--- index.js
+++ index.js
@@ -4,3 +4,8 @@ document.addEventListener("DOMContentLoaded", function() {
     let pText = document.getElementById("p#text")
     text.innerHTML = "This is really cool!";    
   });
+
+  /*
+  The DOMContentLoaded event is now a widely accepted standard. Modern web development, however, provides us with additional choices for setting up when we want our JavaScript to execute. For example, HTML5 now has a defer attribute for use in <script> tags:
+  <script src="index.js" defer></script>
+  */
